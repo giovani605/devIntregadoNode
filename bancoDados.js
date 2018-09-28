1dbbfc2c04f377ae89a688715abf51c970c0442e
Giovani Bossoni <giovanibossoni@hotmail.com>
2018-09-28 16:34:49 -0300

    configuração do banco

--- bancoDados.js
+++ bancoDados.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "200.134.10.143:4567",
+  host: "200.134.10.143:3306",
   user: "Vampira",
   password: "@Vampira#"
 });
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+ con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+ });
