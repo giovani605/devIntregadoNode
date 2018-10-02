eb5828e9407648493cf5630ee18a8ce2c0118251
Giovani <giovanibossoni@hotmail.com>
2018-10-02 15:38:59 -0300

    Implementado headers para se utilizar no angular

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/teste',function(req,res,next){
-    console.log("teste funcionando");
-    res.status(200).send("oi");
+// para poder mandar dados para o angualr
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Header",
+     "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS");
+    console.log("Headers funionando");
+    next();
 });
 
-app.use(function(req,res,next){
-    console.log("teste funcionando");
-    res.status(200).send("oi");
+
+app.get('/teste', function (req, res, next) {
+    res.status(200).json({
+        "msg" : "mensagem"
+    });
 });
+
 
 module.exports = app;
