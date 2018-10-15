64e457a51ff004acf4f83cb8bbe51720e5156632
Giovani <giovanibossoni@hotmail.com>
2018-10-14 21:37:05 -0300

    adicionando comentarios

--- api/rotas/cartoes/CartaoRestApi.js
+++ api/rotas/cartoes/CartaoRestApi.js
@@ -18,4 +18,39 @@ router.get("/user/:id",(req, res, next) => {
     });
 });
 
+// Fazer essa funcao
+function calcularSaldoPeriodo(cartao,inicio,fim){
+    // testar validade das varaiveis
+
+    // como fazer?
+    // consultar transações por periodo
+
+    // calcular um periodo é facil porem
+    // preciso garantir que o valor que tenho antes esta certo
+    // salvar o saldo do cartao a cada x periodo
+        // pode ser a cada mes
+    // recuperar o cache mais recente
+        // Testar se o cache é valido - caso invalido recalcular dinamicamente recursivo
+    // secrementamente sempre colocar o inciio do periodo para o dia em o cache do saldo é criado, quando possivel.
+}
+
+
+// quando criar uma nova transacao
+// talvez nao seja necessario implementar
+function realizarTransfencia(){
+
+}
+
+
+// Calcula o cache de um periodo e salva
+// essa funcao eh a base para criar o cache do saldo
+function calcularCacheSaldo(){
+
+}
+// Vai calcular o saldo desde o inicio da conta
+function calcularCacheSaldoAll(){
+
+}
+
+
 module.exports = router;
