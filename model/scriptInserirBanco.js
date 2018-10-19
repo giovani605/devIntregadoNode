c09b654c5e45308710d60f99da3ffedc180267bf
Giovani Bossoni <giovanibossoni@hotmail.com>
2018-10-19 14:06:35 -0300

    asdas

--- model/scriptInserirBanco.js
+++ model/scriptInserirBanco.js
@@ -11,6 +11,7 @@ var arrayNomes = ["ana", "alexandre", "bura", "giovani", "egon", "priscila"];
 
 for (var a = 0; a < 10; a++) {
     // inserir usuario;
+    // teste
     inserirUsuario();
     // recuperar o nome do usuario e os dados
     // inserir cartoes
